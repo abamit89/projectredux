a6a8351ab5d92480846a96e5fffa57076fbd9037
abamit89 <abamit89@gmail.com>
2021-10-25 20:19:23 +0530

    Update HomeContainer.js

--- src/containers/HomeContainer.js
+++ src/containers/HomeContainer.js
@@ -1,13 +1,18 @@
+import { connect } from 'react-redux';
+
 import Home from '../components/Home';
+import {addToCart,removeToCart} from '../Services/Actions/actions';
 
-import React from 'react'
+const mapStateToProps = state=>({
+    cardData : state.cardItems
+})
 
-function HomeContainer() {
-    return (
-        <div>
-            <Home/>
-        </div>
-    )
-}
+const mapDispatchToProps = dispatch=>({
+    addToCartHandler:data=>dispatch(addToCart(data)),
+    removeToCartHandler:data=>dispatch(removeToCart(data))
+})
 
-export default HomeContainer
+export default connect(mapStateToProps, mapDispatchToProps)(Home);
+
+
+//export default Home
